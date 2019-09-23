c671227fbf16b41d27ed21b95575d5f62945a4ef
gawor <micha.j.gawor@gmail.com>
2019-09-23 16:11:29 +0200

    Set run parameters to be required

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from clartable import Clartable
 from reader import read_data, read_rules
 
 parser = argparse.ArgumentParser(description='Create html table from given data and rules')
-parser.add_argument('-i', metavar='PATH', help='path to input csv file')
-parser.add_argument('-r', metavar='PATH', help='path to json file with rules')
-parser.add_argument('-o', metavar='PATH', help='path to file where output html table will be written')
+parser.add_argument('-i', metavar='PATH', required=True, help='path to input csv file')
+parser.add_argument('-r', metavar='PATH', required=True, help='path to json file with rules')
+parser.add_argument('-o', metavar='PATH', required=True, help='path to file where output html table will be written')
 
 args = parser.parse_args()
 
