65bfc0b9a62bbd5e4a2b006cc41baf79b8f43291
gawor <micha.j.gawor@gmail.com>
2019-09-23 16:21:43 +0200

    artifact removed

--- reader.py
+++ reader.py
@@ -7,9 +7,6 @@ def read_rules(path_to_rules):
         rules = json.load(json_file)
     return rules
 
-def _ret_none():
-    return None
-
 def read_data(path_to_data):
     with open(path_to_data, 'r') as data_file:
         data = pd.read_csv(path_to_data, sep=',', engine='python', keep_default_na=False, na_values=[None])
