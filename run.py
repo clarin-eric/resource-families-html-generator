8fee05f58270866f483110897f3e0c6bfdb5a644
gawor <micha.j.gawor@gmail.com>
2019-10-21 16:31:57 +0200

    single file and nested directory processing, .swp in gitignore

--- run.py
+++ run.py
@@ -15,18 +15,27 @@ if __name__ == "__main__":
     rules = read_rules(args.r)
     clartable = Clartable(rules)
     output = open(args.o, 'w')
-    for root, subdir, files in os.walk(args.i):
-        subdir.sort()
-        if len(files) > 0:
-            if os.path.basename(root) != '':
-                title = '<h2 id="' + '-'.join(os.path.basename(root).split(' ')) + '">' + os.path.basename(root) + '</h2>\n'
-                output.write(title)
-            for _file in files:
-                data = read_data(os.path.join(root, _file))
-                # generate table:
-                if _file != '':
-                    table = "<h3 id=\"table-title\">" + _file.replace('.csv', '') + "</h3>\n"
-                else:
-                    table = ''
-                table += clartable.generate(data)
-                output.write(table)
+
+    # input is a single file
+    if os.path.isfile(args.i):
+        data = open(args.i, 'r')
+        title = <h3 id=\"table-title\">" + os.path.basename(args.i).replace('.csv', '') + "</h3>\n"
+        table = title + clartable.generate(data)
+        output.write(table)
+    # input is a folder
+    else:
+        for root, subdir, files in os.walk(args.i):
+            subdir.sort()
+            if len(files) > 0:
+                if os.path.basename(root) != '':
+                    title = '<h2 id="' + '-'.join(os.path.basename(root).split(' ')) + '">' + os.path.basename(root) + '</h2>\n'
+                    output.write(title)
+                for _file in files:
+                    data = read_data(os.path.join(root, _file))
+                    # generate table:
+                    if _file != '':
+                        table = "<h3 id=\"table-title\">" + _file.replace('.csv', '') + "</h3>\n"
+                    else:
+                        table = ''
+                    table += clartable.generate(data)
+                    output.write(table)
