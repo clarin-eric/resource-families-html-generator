f5ed5a93f42b65560f3d599dd64a496dcbc22dee
Travis CI <nobody@nobody.org>
2019-10-24 18:40:03 +0200

    travis gh-pages deployment

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -6,6 +6,12 @@ set -o errexit
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
-# deploy
+# generate tables
 for D in ./resource_families/*/; do [ -d "$D" ] && ./run.py -i "$D" -o "$D" -r ./rules.json; done
 
+# deploy to tables branch
+cd tables
+git init
+git add .
+git commit -m "Generate tables"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}" master:gh-pages > /dev/null 2>&1
