85c623dae98916c46b5cfb529a12364336602f62
Travis CI <nobody@nobody.org>
2019-10-27 18:51:36 +0100

    run

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -9,6 +9,6 @@ git config --global user.name "Travis CI"
 # deploy to tables branch
 git add .
 git commit -m "Generate tables"
-echo "https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}"
-git push --force "https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}" travis
-# > /dev/null 2>&1
+echo "https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git"
+echo "___"
+git push --force "https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}" travis:gh-pages > /dev/null 2>&1
