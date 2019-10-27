e585e44d0097c9aca8c295574c9636198f8a6ac1
Travis CI <nobody@nobody.org>
2019-10-27 20:23:30 +0100

    test travis auto deploy

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -5,8 +5,3 @@ set -o errexit
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
-
-
-git add .
-git commit -m "Generate tables"
-git push --force https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO} travis:gh-pages > /dev/null 2>&1
