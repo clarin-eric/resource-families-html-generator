bd0e5c3dc49768a9a34daf1aa500a5ee98bcf976
Travis CI <nobody@nobody.org>
2019-10-27 20:06:33 +0100

    do not initialise repo

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -6,9 +6,7 @@ set -o errexit
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
-# deploy to tables branch
-cd tables
-git init
+
 git add .
 git commit -m "Generate tables"
 git push --force https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO} travis:gh-pages > /dev/null 2>&1
