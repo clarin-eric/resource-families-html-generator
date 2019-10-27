63ba3b39d6d62baa950bdab8bb2055a1b25d596a
Travis CI <nobody@nobody.org>
2019-10-27 20:03:32 +0100

    test auth

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -7,8 +7,8 @@ git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
 # deploy to tables branch
+cd tables
+git init
 git add .
 git commit -m "Generate tables"
-echo https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git
-echo "___" 
-#git push --force "https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}" travis:gh-pages > /dev/null 2>&1
+git push --force https://github-ci-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO} travis:gh-pages > /dev/null 2>&1
