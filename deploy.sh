1ce8a176da06b7eddd96a36641bec7bda236ec6b
Travis CI <nobody@nobody.org>
2019-10-28 13:34:17 +0100

    init repo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 git config --global user.email "CI-Bot@travis.com"
 git config --global user.name "Travis Bot"
 
+git init
 git add .
 git commit -m "Deploy tables"
 git push --force https://$GITHUB_TOKEN:x-oauth-basic@github.com/clarin-eric/resource-families-html-generator.git travis:gh-pages
