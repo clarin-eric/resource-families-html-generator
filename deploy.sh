85f9ea1813c54c288b74854124efb0413506eecc
Travis CI <nobody@nobody.org>
2019-10-28 13:30:31 +0100

    force push to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git config --global user.name "Travis Bot"
 
 git add .
 git commit -m "Deploy tables"
-git push https://$GITHUB_TOKEN:x-oauth-basic@github.com/clarin-eric/resource-families-html-generator.git travis
+git push --force https://$GITHUB_TOKEN:x-oauth-basic@github.com/clarin-eric/resource-families-html-generator.git travis:gh-pages
 
