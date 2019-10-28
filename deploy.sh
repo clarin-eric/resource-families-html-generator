efefca7376af7abc4a67681f4e1172ed27e8b948
Travis CI <nobody@nobody.org>
2019-10-28 13:18:57 +0100

    typo

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git config --global user.email "CI-Bot@travis.com"
 git config --global user.name "Travis Bot"
 
-git add ./tables > 2>&1
+git add ./tables
 git commit -m "Deploy tables"
 git push https://$GITHUB_TOKEN:x-oauth-basic@github.com/clarin-eric/resource-families-html-generator.git travis
 
