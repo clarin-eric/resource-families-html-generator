cd08fb2bc7ff8577fa81a2dedda23703a37b7edd
MichalGawor <michal@clarin.eu>
2019-11-06 18:09:13 +0100

    Trap 1 instead of EXIT

--- generate-tables.sh
+++ generate-tables.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#/bin/bash
 
 set -e
 
 for D in ./resource_families/*/; do 
 	[ -d "$D" ] && ./run.py -i "$D" -o "$D" -r ./rules.json
-	trap "echo Corpora that failed to process: $D" EXIT
+	trap "echo Corpora that failed to process: $D" 1 
 done
 for D in ./tables/*; do echo $D; done
