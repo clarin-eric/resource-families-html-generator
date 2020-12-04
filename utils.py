bf3e5f8a45fbf4a4cf3853756d09ac0795d38885
Gawor,M.J. (Michał) <m.j.gawor@students.uu.nl>
2020-12-04 12:41:20 +0100

    change separator from , to ;

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def read_rules(path_to_rules):
 
 def read_data(path_to_data):
     with open(path_to_data, 'r') as data_file:
-        data = pd.read_csv(path_to_data, sep=',', engine='python', keep_default_na=False, na_values=[None])
+        data = pd.read_csv(path_to_data, sep=';', engine='python', keep_default_na=False, na_values=[None])
     return data
 
 def get_title(file_path):
